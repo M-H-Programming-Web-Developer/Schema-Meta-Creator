274dd20128fdecf729d9dcb4e11f8b8ac5c9c6ce
Mohamed Hassan <66780217+M-H-Programming-Web-Developer@users.noreply.github.com>
2026-01-20 17:28:35 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // This ensures your output goes where Vercel expects it
+    outDir: 'dist',
+  }
 })
